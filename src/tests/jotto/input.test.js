3c7e86b6f0228a410ccac4a717cfd08b593d4e9f
Bradley <bbarnes.me@gmail.com>
2019-12-30 01:24:36 -0600

    Add input component and storeFactory to testUtils

---
+++ src/tests/jotto/input.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, storeFactory } from "../../../test/testUtils";
+import Input from "../../components/jotto/input";
+
+const setup = (preloadedState={}) => {
+  const store = storeFactory(preloadedState);
+  return shallow(<Input store={store}/>).dive().dive().debug();
+};
+
+setup(); 
+
+
+describe("render", () => {
+  describe("When word has not been guessed", () => {
+    test("renders without error", () => {
+
+    });
+
+    test("renders the input box", () => {
+
+    });
+
+    test("renders the submit button", () => {
+
+    });
+  });
+  
+  describe("When word has been guessed", () => {
+    test("renders without error", () => {
+
+    });
+
+    test("does not render the input box", () => {
+
+    });
+
+    test("does not render the submit button", () => {
+
+    });
+  });
+});
+
+describe("update the state", () => {
+
+});
+
+test("Has access to succes part of state in props", () => {
+
+});
+
+test("Has correctGuess action creator as prop", () => {
+
+});
+
+test("Has addGuess action creator as prop", () => {
+
+});
+
+
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,10 @@
 import checkPropTypes from "check-prop-types";
+import rootReducer from "../src/reducers/rootReducer";
+import { createStore } from "redux";
+
+export const storeFactory = (preloadedState) => (
+  createStore(rootReducer, preloadedState)
+);
 
 export const findByTestAttr = (wrapper, attribute) => (
   wrapper.find(`[data-test='${attribute}']`)
