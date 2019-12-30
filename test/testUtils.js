b3e5fd44697f3e9e0d7ab45428ed70ea2a5f3bd7
Bradley <bbarnes.me@gmail.com>
2019-12-30 13:06:56 -0600

    Add redux thunk and logger to storeFactory in testUtils

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,11 @@
 import checkPropTypes from "check-prop-types";
 import rootReducer from "../src/reducers/rootReducer";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const storeFactory = (preloadedState) => (
-  createStore(rootReducer, preloadedState)
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
 );
 
 export const findByTestAttr = (wrapper, attribute) => (
