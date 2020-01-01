362af01029319756017800be096573dc68b756e4
Bradley <bbarnes.me@gmail.com>
2019-12-31 20:25:01 -0600

    Add code and tests for jotto component and connect jotto component to store

--- src/components/jotto/jotto.js
+++ src/components/jotto/jotto.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import Congrats from "./congrats";
 import GuessedWords from "./guessedWords";
 import Input from "./input";
+import { getSecretWord } from "../../actions/actions";
 
-const Jotto = () => {
-  return(
-    <div className="container">
-      <h1>Jotto</h1>
-      <Congrats success={true}/>
-      <Input/>
-      <GuessedWords 
-        guessedWords={[{
-          guessedWord: "train", 
-          letterMatchCount: 5
-        }]}
-        />
-    </div>
-  )     
+class Jotto extends Component {
+  render(){
+    
+    return(
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success}/>
+        <Input/>
+        <GuessedWords 
+          guessedWords={this.props.guessedWords}
+          />
+      </div>
+    )
+  } 
 };
 
-export default Jotto;
+const msp = ( { success, guessedWords, secretWord } ) => ({
+  success,
+  guessedWords,
+  secretWord
+})
+
+export default connect(msp, { getSecretWord })(Jotto);
