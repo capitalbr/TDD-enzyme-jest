b3e63c6a96bc887ab6882f1b4c20473956492236
Bradley <bbarnes.me@gmail.com>
2020-01-01 19:32:55 -0600

    Remove enzyme mount function from guessedWords.test.js

--- src/tests/jotto/guessedWords.test.js
+++ src/tests/jotto/guessedWords.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../../../test/testUtils";
 import GuessedWords from "../../components/jotto/guessedWords";
 
